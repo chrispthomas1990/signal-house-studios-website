5033d69e20d5e20d48a204a0dd33e0ea06eb73a3
Chris Thomas <ChrisThomasMac@chris-thomas-macbook-pro.local>
2026-05-27 13:51:51 +0100

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,39 @@ import { VideoProduction } from "./pages/VideoProduction";
 import { LiveStreaming } from "./pages/LiveStreaming";
 import { Contact } from "./pages/Contact";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "audio-production",
+          element: <AudioProduction />,
+        },
+        {
+          path: "video-production",
+          element: <VideoProduction />,
+        },
+        {
+          path: "live-streaming",
+          element: <LiveStreaming />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "audio-production",
-        element: <AudioProduction />,
-      },
-      {
-        path: "video-production",
-        element: <VideoProduction />,
-      },
-      {
-        path: "live-streaming",
-        element: <LiveStreaming />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
+    basename: "/signal-house-studios-website",
   },
-]);
+);
 
 function App() {
   return <RouterProvider router={router} />;
